575fdc168cef7fbc5ceee4dfd01934d212590206
alansilva313 <allansillva313@gmail.com>
2024-11-18 10:47:02 -0300

    capa

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,34 @@
-
 import Categorias from "./components/categorias";
 import Produtos from "./components/produtos";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import hbcapa from "./assets/capa_hb.jpg";
 
 function App() {
-
-
-
   return (
-    <div className="w-full h-screen">
+    <div className="w-full h-screen flex flex-col">
+      {/* Cabeçalho com background */}
+      <div
+        className="w-full h-[150px] relative flex items-center justify-between bg-cover bg-center"
+        style={{ backgroundImage: `url(${hbcapa})` }}
+      >
+        <div className="w-full flex items-center justify-center">
+          {/* Avatar */}
+          <Avatar className="w-[95px] h-[95px] border bg-slate-100 flex items-center justify-center rounded-full top-24 absolute">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+      </div>
 
+      {/* Categorias */}
+      <Categorias />
 
-      <div className="w-full h-[180px] bg-orange-400 relative flex items-center justify-between">
-          
-    
-    <div className="w-full flex items-center justify-center">
-    <div className="w-[95px] h-[95px] border bg-slate-100 flex items-center justify-center rounded-full top-32 absolute">
-         <p className="text-2xl">AS</p>
+      {/* Produtos */}
+      <div className="mt-16 p-4 flex flex-col gap-4 overflow-y-auto">
+        <Produtos />
       </div>
     </div>
-      </div>
-
-        
-     <Categorias />
-
-      <div className="mt-12 p-2 flex flex-col gap-2 h-screen">
-  
-
-      
-
-       <Produtos />
-        
-      </div>
-
-   
-   
-
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
