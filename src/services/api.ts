404a3b8fd38ef4168e4f5d1c6fa83194d697016e
alansilva313 <allansillva313@gmail.com>
2024-11-18 11:23:34 -0300

    ajustes

--- src/services/api.ts
+++ src/services/api.ts
@@ -28,6 +28,16 @@ export const api = [
         price: 39.90,
         category: "Bebidas"
     },
+
+    {
+        id: 4,
+        title: "Hmaburger + bata frita + refri lata",
+        description: "Hamburger caseiro delicioso",
+        image: "",
+        ingredients: "hamburger, salsicha, alface, picles, milho, azeitona",
+        price: 24.90,
+        category: "Combos"
+    },
   
   
    
